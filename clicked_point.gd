16b1ee8eed43e7fcb3c99a6c0969553587ea0363
lethalgem <iwc123@gmail.com>
2024-01-13 16:44:50 -0600

    Fix compile errors

--- clicked_point.gd
+++ clicked_point.gd
@@ -1,36 +1,5 @@
 extends CharacterBody2D
 
-var is_click_held = false
-var click_position = Vector2(0,0)
 
 func _ready():
-	click_position = Vector2(position.x, position.y)
-	%MuzzleFlash.hide()
-
-func _physics_process(_delta):
-	if Input.is_action_just_pressed("left_click"):
-		is_click_held = true
-		click_position = get_global_mouse_position()
-		global_position = click_position
-		Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
-		%MuzzleFlash.show()
-	elif Input.is_action_just_released("left_click"):
-		is_click_held = false
-		%MuzzleFlash.hide()
-		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-
-	if is_click_held:
-		var distance_to_held_position = calculate_distance(click_position, get_global_mouse_position())
-		if distance_to_held_position > 200:
-			%ClickArrow.show()
-	else:
-		%ClickArrow.hide()
-
-
-func calculate_distance(position_one: Vector2, position_two: Vector2) -> float:
-	var dx = position_two.x - position_one.x
-	var dy = position_two.y - position_one.y
-	var distance = sqrt(dx**2 + dy**2)
-	return distance
-
-
+	%HappyBoo.play_idle_animation()
--- mob.gd
+++ mob.gd
@@ -2,15 +2,18 @@ extends CharacterBody2D
 
 var health = 3
 
-@onready var player = get_node("/root/Game/Player")
+@onready var player = get_node("/root/Game/Goku")
+
 
 func _ready():
 	%Slime.play_walk()
+
 
 func _physics_process(delta):
 	var direction = global_position.direction_to(player.global_position)
 	velocity = direction * 300.0
 	move_and_slide()
+
 
 func take_damage():
 	health -= 1
