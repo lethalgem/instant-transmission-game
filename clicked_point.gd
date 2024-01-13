2f72ea8886084fa168f1a44b4cfb75164de396a3
lethalgem <iwc123@gmail.com>
2024-01-13 15:37:40 -0600

    Have an arrow point to where the mouse is dragged

--- clicked_point.gd
+++ clicked_point.gd
@@ -12,16 +12,26 @@ func _physics_process(delta):
 		is_click_held = true
 		click_position = get_global_mouse_position()
 		global_position = click_position
-		Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+		#Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 		%MuzzleFlash.show()
 	elif Input.is_action_just_released("left_click"):
 		print("UP")
 		is_click_held = false
 		%MuzzleFlash.hide()
-		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		#Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 	if is_click_held:
+		var distance_to_held_position = calculate_distance(click_position, get_global_mouse_position())
+		if distance_to_held_position > 200:
+			%ClickArrow.show()
+	else:
+		%ClickArrow.hide()
 
 
+func calculate_distance(position_one: Vector2, position_two: Vector2) -> float:
+	var dx = position_two.x - position_one.x
+	var dy = position_two.y - position_one.y
+	var distance = sqrt(dx**2 + dy**2)
+	return distance
 
 
