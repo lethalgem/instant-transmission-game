c02514175eef27264d2007c7279be417babfe259
lethalgem <iwc123@gmail.com>
2024-01-14 16:28:46 -0600

    kill player

--- goku.gd
+++ goku.gd
@@ -1,11 +1,24 @@
 extends CharacterBody2D
 
 var is_frozen = false
+var health = 1
 
 
 func _ready():
 	%HappyBoo.play_idle_animation()
 	print(%TeleportPreventionCollision2D.get_path())
+
+
+func take_damage():
+	health -= 1
+
+	if health == 0:
+		queue_free()
+
+		const SMOKE_SCENE = preload("res://smoke_explosion/smoke_explosion.tscn")
+		var smoke = SMOKE_SCENE.instantiate()
+		get_parent().add_child(smoke)
+		smoke.global_position = global_position
 
 
 func _on_clicked_point_click_released(
